0a07f1a257ba866dd8f0680a591abafadc472604
unknown <gabrielmartinbuzatto1999@gmail.com>
2022-08-08 15:08:14 -0300

    Comecando adicionar pecas no tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 namespace Xadrez
 {
     class Program
@@ -7,6 +8,10 @@ namespace Xadrez
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
+            tab.colocarPeca(new Cavalo(tab,Cor.Preta), new Posicao(0,1));
+            tab.colocarPeca(new Bispo(tab,Cor.Preta), new Posicao(0,2));
+            tab.colocarPeca(new Piao(tab, Cor.Preta), new Posicao(1, 0));
             Tela.imprimirtabuleiro(tab);            
         }
     }
